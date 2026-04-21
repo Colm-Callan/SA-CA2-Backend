bb9bc8cfabf0f33927e89b1fa0af8418fff646c9
ccallan04 <colmcallan04@gmail.com>
2026-04-21 18:18:07 +0100

    fixing deploy

--- SACA2/SACA2/Program.cs
+++ SACA2/SACA2/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using SACA2.Data;
-using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +7,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Database configuration
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Railway provides DATABASE_URL automatically
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL")
+    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
